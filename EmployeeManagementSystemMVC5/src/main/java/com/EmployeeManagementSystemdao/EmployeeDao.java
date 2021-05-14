8e41f302f02ed1e6f7bbfc3830aa158ac9636866
vinita <imvinitarajoria@gmail.com>
2021-05-14 16:42:22 +0530

    MVC 5

---
+++ EmployeeManagementSystemMVC5/src/main/java/com/EmployeeManagementSystemdao/EmployeeDao.java
@@ -0,0 +1,50 @@
+package com.EmployeeManagementSystemdao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import com.EmployeeManagementSystembeans.Employee;
+
+public class EmployeeDao {
+
+	JdbcTemplate template;  
+	  
+	public void setTemplate(JdbcTemplate template) {  
+	    this.template = template;  
+	}  
+	public int save(Employee p){  
+	    String sql="insert into Employee99(name,salary,designation) values('"+p.getName()+"',"+p.getSalary()+",'"+p.getDesignation()+"')";  
+	    return template.update(sql);  
+	}  
+	public int update(Employee p){  
+	    String sql="update Employee99 set name='"+p.getName()+"', salary="+p.getSalary()+",designation='"+p.getDesignation()+"' where id="+p.getId()+"";  
+	    return template.update(sql);  
+	}  
+	public int delete(int id){  
+	    String sql="delete from Employee99 where id="+id+"";  
+	    return template.update(sql);  
+	}  
+	@SuppressWarnings("deprecation")
+	public Employee getEmployeeById(int id){  
+	    String sql="select * from Employee99 where id=?";  
+	    return template.queryForObject(sql, new Object[]{id},new BeanPropertyRowMapper<Employee>(Employee.class));  
+	}  
+	public List<Employee> getEmployees(){  
+	    return template.query("select * from Employee99",new RowMapper<Employee>(){  
+	        public Employee mapRow(ResultSet rs, int row) throws SQLException {  
+	        	Employee e=new Employee();  
+	            e.setId(rs.getInt(1));  
+	            e.setName(rs.getString(2));  
+	            e.setSalary(rs.getFloat(3));  
+	            e.setDesignation(rs.getString(4));  
+	            return e;  
+	        }  
+	    });  
+	}  	
+	
+}
---
+++ EmployeeManagementSystemMVC5/src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<a href="empform.">Add Employee</a>
+<a href="viewemp">View Employees</a>
+
+
+
